d84103416f3bc3f4b041e1970f6943e3fda0a316
Luke Alford <laa1010@gmail.com>
2012-07-11 01:39:55 -0400

    grocery list foods to purchase now displayed in store sections

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -25,8 +25,9 @@ class GroceryList < ActiveRecord::Base
   #     "unit_name" => "unit name 2" },
   #   ...
   # }
+  NO_SECTION_KEY = "No Section"
   QUANTITY_KEY = "quantity"
-  UNIT_NAME_KEY = "unit_name"
+  UNIT_NAME_KEY = "unit name"
   def generate
     generated_list = {}
 
@@ -35,13 +36,21 @@ class GroceryList < ActiveRecord::Base
       recipe_quantity = glr.quantity
       recipe = glr.recipe
       recipe.ingredients.each do |ingredient|
-        merge_purchase_items(generated_list, ingredient.food.name, (recipe_quantity * ingredient.quantity), ingredient.unit)
+        puts "before default, recipe quantity = #{recipe_quantity}"
+        puts "before default, ingredient quantity = #{ingredient.quantity}"
+        recipe_quantity = 1 if !recipe_quantity
+        ingredient_quantity = ingredient ? ingredient.quantity : 1
+        puts "after default, recipe quantity = #{recipe_quantity}"
+        puts "after default, ingredient quantity = #{ingredient_quantity}"
+
+        quantity = (recipe_quantity * ingredient_quantity)
+        merge_purchase_items(generated_list, ingredient.food, quantity, ingredient.unit)
       end if recipe and recipe.ingredients
     end if grocery_list_recipes
 
     # add a la carte foods
     grocery_list_foods.each do |glf|
-      merge_purchase_items(generated_list, glf.food.name, glf.quantity, glf.unit)
+      merge_purchase_items(generated_list, glf.food, glf.quantity, glf.unit)
     end if grocery_list_foods
 
     generated_list
@@ -62,24 +71,40 @@ class GroceryList < ActiveRecord::Base
     unit_name
   end
 
+  def set_store_section_display_name(food)
+    if food.store_section and food.store_section.name and !food.store_section.name.blank?
+      store_section_name = food.store_section.name
+    else
+      store_section_name = NO_SECTION_KEY
+    end
+    store_section_name
+  end
+
   def build_food_item_hash(food_name, quantity, unit)
     unit_name = set_unit_display_name(unit)
     { food_name => { QUANTITY_KEY => quantity, UNIT_NAME_KEY => unit_name } }
   end
 
-  def merge_purchase_items(current_list, food_name, quantity, unit)
-    current_list.merge!(build_food_item_hash(food_name, quantity, unit)) { |food_name, current_food_info, next_food_info|
-      if current_food_info[UNIT_NAME_KEY] == next_food_info[UNIT_NAME_KEY]
-        puts "for food #{food_name}, adding additional quantity #{next_food_info[QUANTITY_KEY]} to current quantity #{current_food_info[QUANTITY_KEY]}"
-        new_food_info = { QUANTITY_KEY => (current_food_info[QUANTITY_KEY] + next_food_info[QUANTITY_KEY]),
-          UNIT_NAME_KEY => current_food_info[UNIT_NAME_KEY] }
-      else
-        puts "for food #{food_name}, unit names are mismatched so just printing the addition string without conversion for now"
-        new_food_info = { QUANTITY_KEY => "#{current_food_info[QUANTITY_KEY]} + #{next_food_info[QUANTITY_KEY]}",
-          UNIT_NAME_KEY => "#{current_food_info[UNIT_NAME_KEY]} + #{next_food_info[UNIT_NAME_KEY]}" }
-      end
-      puts "new food info:\n#{new_food_info}"
-      new_food_info
+  def build_store_section_hash(food, quantity, unit)
+    store_section_name = set_store_section_display_name(food)
+    { store_section_name => build_food_item_hash(food.name, quantity, unit) }
+  end
+
+  def merge_purchase_items(current_list, food, quantity, unit)
+    current_list.merge!(build_store_section_hash(food, quantity, unit)) { |store_section_name, current_section_food_hash_set, next_section_food_hash_set|
+      current_section_food_hash_set.merge(next_section_food_hash_set) { |food_name, current_food_info, next_food_info|
+        if current_food_info[UNIT_NAME_KEY] == next_food_info[UNIT_NAME_KEY]
+          puts "for food #{food_name}, adding additional quantity #{next_food_info[QUANTITY_KEY]} to current quantity #{current_food_info[QUANTITY_KEY]}"
+          new_food_info = { QUANTITY_KEY => (current_food_info[QUANTITY_KEY] + next_food_info[QUANTITY_KEY]),
+            UNIT_NAME_KEY => current_food_info[UNIT_NAME_KEY] }
+        else
+          puts "for food #{food_name}, unit names are mismatched so just printing the addition string without conversion for now"
+          new_food_info = { QUANTITY_KEY => "#{current_food_info[QUANTITY_KEY]} + #{next_food_info[QUANTITY_KEY]}",
+            UNIT_NAME_KEY => "#{current_food_info[UNIT_NAME_KEY]} + #{next_food_info[UNIT_NAME_KEY]}" }
+        end
+        puts "new food info:\n#{new_food_info}"
+        new_food_info
+      }
     }
   end
 end
--- test/unit/grocery_list_test.rb
+++ test/unit/grocery_list_test.rb
@@ -55,40 +55,66 @@ class GroceryListTest < ActiveSupport::TestCase
   #     spinach, 1, bag
   #
   # generated_list:
-  #   pesto, 16, tablespoon
-  #   pine nuts, 1 + 10, bag + ounce
-  #   plum, 3, blank
-  #   salmon, 4, filet
-  #   spinach, 2, bag
-  #   sundried tomato, 1, tablespoon
+  #   no section
+  #     pesto, 16, tablespoon
+  #     pine nuts, 1 + 10, bag + ounce
+  #     sundried tomato, 1, tablespoon
+  #   produce
+  #     plum, 3, blank
+  #     spinach, 2, bag
+  #   seafood
+  #     salmon, 4, filet
   #
   test "should generate the grocery list hash from fixture data" do
     grocery_list = grocery_lists(:l1)
     generated_list = grocery_list.generate
     assert_not_nil generated_list, "should have built a grocery list"
-    assert_equal 6, generated_list.keys.count, "should have had 6 foods in the list"
+    assert_equal 3, generated_list.keys.count, "should have had 3 store sections in this list"
     puts "whole generated list:\n#{generated_list}"
-    generated_list.keys.each do |food_name|
+    validate_generated_list generated_list
+  end
+
+  def validate_generated_list(generated_list)
+    assert_not_nil generated_list
+    generated_list.keys.sort.each do |store_section_name|
+      puts "store section key = #{store_section_name}"
+      section_food_list = generated_list[store_section_name]
+      case store_section_name
+        when GroceryList::NO_SECTION_KEY
+          assert_equal 3, section_food_list.keys.count, "should have had 3 foods without a store section defined"
+        when store_sections(:produce).name
+          assert_equal 2, section_food_list.keys.count, "should have had 2 foods in the produce section"
+        when store_sections(:seafood).name
+          assert_equal 1, section_food_list.keys.count, "should have had 1 food in the seafood section"
+        else
+          assert false, "section #{store_section_name} was not expected in the list"
+      end
+      validate_section_food_list section_food_list
+    end
+  end
+
+  def validate_section_food_list(section_food_list)
+    section_food_list.keys.each do |food_name|
       puts "food key = #{food_name}"
       case food_name
         when foods(:pesto).name
-          assert_equal 16, generated_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
-          assert_equal units(:tbsp).long_name, generated_list[food_name][GroceryList::UNIT_NAME_KEY], "expected unit name to match"
+          assert_equal 16, section_food_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
+          assert_equal units(:tbsp).long_name, section_food_list[food_name][GroceryList::UNIT_NAME_KEY], "expected unit name to match"
         when foods(:pine_nuts).name
-          assert_equal "1.0 + 10.0", generated_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
-          assert_equal "#{units(:bag).short_name} + #{units(:oz).long_name}", generated_list[food_name][GroceryList::UNIT_NAME_KEY], "expected unit name to match"
+          assert_equal "1.0 + 10.0", section_food_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
+          assert_equal "#{units(:bag).short_name} + #{units(:oz).long_name}", section_food_list[food_name][GroceryList::UNIT_NAME_KEY], "expected unit name to match"
         when foods(:plum).name
-          assert_equal 3, generated_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
-          assert_blank generated_list[food_name][GroceryList::UNIT_NAME_KEY], "expected no unit"
+          assert_equal 3, section_food_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
+          assert_blank section_food_list[food_name][GroceryList::UNIT_NAME_KEY], "expected no unit"
         when foods(:salmon).name
-          assert_equal 4, generated_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
-          assert_equal units(:filet).short_name, generated_list[food_name][GroceryList::UNIT_NAME_KEY], "expected unit name to match"
+          assert_equal 4, section_food_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
+          assert_equal units(:filet).short_name, section_food_list[food_name][GroceryList::UNIT_NAME_KEY], "expected unit name to match"
         when foods(:spinach).name
-          assert_equal 2, generated_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
-          assert_equal units(:bag).short_name, generated_list[food_name][GroceryList::UNIT_NAME_KEY], "expected unit name to match"
+          assert_equal 2, section_food_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
+          assert_equal units(:bag).short_name, section_food_list[food_name][GroceryList::UNIT_NAME_KEY], "expected unit name to match"
         when foods(:sundried_tomato).name
-          assert_equal 1, generated_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
-          assert_equal units(:tbsp).long_name, generated_list[food_name][GroceryList::UNIT_NAME_KEY], "expected unit name to match"
+          assert_equal 1, section_food_list[food_name][GroceryList::QUANTITY_KEY], "expected quantities to match"
+          assert_equal units(:tbsp).long_name, section_food_list[food_name][GroceryList::UNIT_NAME_KEY], "expected unit name to match"
         else
           assert false, "food #{food_name} was not expected in the list"
       end
