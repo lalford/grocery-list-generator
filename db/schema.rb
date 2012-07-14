e3ef183e67f018fc27539f6e29d24accc9df212c
Luke Alford <laa1010@gmail.com>
2012-07-13 23:32:02 -0400

    resolve conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120711032044) do
+ActiveRecord::Schema.define(:version => 20120712024247) do
 
   create_table "foods", :force => true do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20120711032044) do
     t.integer  "unit_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "unit_name"
   end
 
   create_table "grocery_lists_recipes", :force => true do |t|
@@ -56,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20120711032044) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "unit_id"
+    t.string   "unit_name"
   end
 
   create_table "labels", :force => true do |t|
