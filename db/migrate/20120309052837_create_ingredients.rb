dedba3022ff94932ccdffbc69e16479de2ac58bb
Luke Alford <laa1010@gmail.com>
2012-07-15 12:46:16 -0400

    trying to fix screwed up migrations

--- db/migrate/20120309052837_create_ingredients.rb
+++ db/migrate/20120309052837_create_ingredients.rb
@@ -4,7 +4,6 @@ class CreateIngredients < ActiveRecord::Migration
       t.column "food_id", :integer, :null => false
       t.column "recipe_id", :integer, :null => false
       t.column "quantity", :float
-      t.column "unit", :string
 
       t.timestamps
     end
