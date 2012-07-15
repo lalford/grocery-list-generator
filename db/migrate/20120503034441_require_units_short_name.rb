ee40871dcf7351a4944e921c94360ffd51423845
Luke Alford <laa1010@gmail.com>
2012-07-14 23:37:39 -0400

    removed bad migration

--- db/migrate/20120503034441_require_units_short_name.rb
+++
@@ -1,9 +0,0 @@
-class RequireUnitsShortName < ActiveRecord::Migration
-  def up
-    change_column :units, :short_name, :string, :null => false
-  end
-
-  def down
-    change_column :units, :short_name, :string, :null => true
-  end
-end
